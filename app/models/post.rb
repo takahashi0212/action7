14bace2ea0c1ca18cc642b32dabc3311f07019d0
takahashi0212 <rt6965@yahoo.co.jp>
2020-09-26 18:53:13 +0900

    バリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,12 @@ class Post < ApplicationRecord
   #validates :category, presence: true
   #カテゴリーの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 0 }
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :category_id
+    validates :price, numericality: {with: /\A[0-9]+\z/}
+  end
 end
