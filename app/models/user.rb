9cc44dac94d079c6b6fb1b86d0ec9163137a17e6
takahashi0212 <rt6965@yahoo.co.jp>
2020-10-05 02:09:28 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  
+  with_options presence: true do
+    validates :nickname, length: { maximum: 20 }
+    validates :email, uniqueness: {case_sensitive: true}, format: { with: VALID_EMAIL_REGEX }
+    validates :password, length: {minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  end
+
   def already_like?(post)
     self.likes.exists?(post_id: post.id)
   end
