994c167159dd121f554593c2b42fa42c183f80b5
takahashi0212 <rt6965@yahoo.co.jp>
2020-10-05 01:59:59 +0900

    編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,16 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:image, :name, :description, :category_id, :price).merge(user_id: current_user.id)
